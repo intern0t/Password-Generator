498003707c3e2666951f59469584c585b63c73dd
Prashant Shrestha <pms@engineer.com>
2015-09-11 19:16:43 -0400

    Cleaned up the variables, added loop & all set to generate.

--- Password Generator/Password Generator/Program.cs
+++ Password Generator/Password Generator/Program.cs
@@ -7,31 +7,39 @@ namespace Password_Generator
 {
     class Program
     {
-        private static string PasswordCharacters = "abcdefgijkmnopqrstwxyzABCDEFGHJKLMNPQRSTWXYZ";
-        private static string PasswordNumbers    = "0123456789";
-        private static string PasswordSpecials   = "*$-+?_&=!%{}/";
-
-        private static string temp               = string.Empty;
-        private static int pwdLength             = 0;
+        private static string PasswordCharacters    = "abcdefgijkmnopqrstwxyzABCDEFGHJKLMNPQRSTWXYZ0123456789*$-+?_&=!%{}/";
+        private static string temp                  = string.Empty;
+        private static int pwdLength                = 0;
 
         static void Main(string[] args)
         {
-            string Options  = 
-                            "Open Source Password Generator by Prashant M. Shrestha.\n" +
-                            "Please type the length of password to generate 10 unique passwords.\n";
+            Console.Title = "Random Password Generator - NonInfectio.us!";
 
-            Write(Options);
+            Write("Open Source Password Generator by Prashant M.Shrestha (Scarecrow) - www.noninfectio.us\n");
 
-            temp            = Console.ReadLine();
+            string Options = "Please type the length of password to generate 10 unique passwords.";
 
-            Int32.TryParse(temp, out pwdLength);
-
-            Write(pwdLength + "");
-
-            if(pwdLength > 0 && pwdLength < 25)
+            do
             {
-                
-            }
+                while (!Console.KeyAvailable)
+                {
+                    Write(Options);
+
+                    temp = Console.ReadLine();
+
+                    Int32.TryParse(temp, out pwdLength);
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+
+                    if (pwdLength > 0 && pwdLength <= 128)
+                    {
+                        // Generate a password ..
+                    }
+                    else { Write("Please have the password length be 1-128." + Environment.NewLine); }
+
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
 
         static void Write(string S)
