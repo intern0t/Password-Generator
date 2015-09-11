36b675323be616d50cecfceb62f44b5032b5a01d
Prashant Shrestha <pms@engineer.com>
2015-09-11 18:33:56 -0400

    Initial project files setup & a method to Console -> Write.

---
+++ Password Generator/Password Generator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Password_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Options = 
+                "Open Source Password Generator by Prashant M. Shrestha.\n" +
+                "Please type the length of password to generate 10 unique passwords.\n";
+
+            Write(Options);
+
+            Console.Read();
+        }
+
+        static void Write(string S)
+        {
+            Console.WriteLine(S);
+        }
+    }
+}
