4a83e2e268bb7498a57e20af6624b9513e12261b
Prashant Shrestha <pms@engineer.com>
2015-09-11 18:53:27 -0400

    Cleaned the variable structures a bit. Keepin' it clean and healthy!

--- Password Generator/Password Generator/Program.cs
+++ Password Generator/Password Generator/Program.cs
@@ -1,20 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Web;
 
 namespace Password_Generator
 {
     class Program
     {
+        private static string PasswordCharacters = "abcdefgijkmnopqrstwxyzABCDEFGHJKLMNPQRSTWXYZ";
+        private static string PasswordNumbers    = "0123456789";
+        private static string PasswordSpecials   = "*$-+?_&=!%{}/";
+
+        private static string temp               = string.Empty;
+        private static int pwdLength             = 0;
+
         static void Main(string[] args)
         {
-            string Options = 
-                "Open Source Password Generator by Prashant M. Shrestha.\n" +
-                "Please type the length of password to generate 10 unique passwords.\n";
+            string Options  = 
+                            "Open Source Password Generator by Prashant M. Shrestha.\n" +
+                            "Please type the length of password to generate 10 unique passwords.\n";
 
             Write(Options);
 
-            Console.Read();
+            temp            = Console.ReadLine();
+
+            Int32.TryParse(temp, out pwdLength);
+
+            Write(pwdLength + "");
+
+            if(pwdLength > 0 && pwdLength < 25)
+            {
+                
+            }
         }
 
         static void Write(string S)
